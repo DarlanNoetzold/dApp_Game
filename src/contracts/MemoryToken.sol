7352c82b972ce8160f4891616f957dd19d68a125
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2022-09-22 06:56:36 -0300

    Update MemoryToken.sol

--- src/contracts/MemoryToken.sol
+++ src/contracts/MemoryToken.sol
@@ -8,6 +8,7 @@ contract MemoryToken is ERC721Full  {
     constructor() ERC721Full("Memory Token", "MEMORY") public {
     }
 
+    //Call the ERC721 function to define de origin
     function mint(address _to, string memory _tokenURI) public returns(bool) {
        uint _tokenId = totalSupply().add(1);
        _mint(_to, _tokenId);
